94689e762247ad22a3de925dc86a380c8871a492
VjeremyV <vaugoyeau.jeremy@gmail.com>
2025-07-12 19:15:01 +0200

    fix deploy

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index() : Response
     {
         return Inertia::render('web/dashboard/index', [
             'title' => 'Dashboard',
