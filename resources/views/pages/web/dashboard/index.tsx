7f306219a1f73e1063a22364195dfd065ca2e4ca
VjeremyV <vaugoyeau.jeremy@gmail.com>
2025-07-14 17:23:14 +0200

    test finali pipeline

--- resources/views/pages/web/dashboard/index.tsx
+++ resources/views/pages/web/dashboard/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
     return (
         <div>
         <h1>Bienvenue</h1>
-        <p>du blabla blbla sur le dashboard</p>
+        <p>du blabla blbla sur le dashboard on voit que ça marche</p>
     </div>
     );
 }
